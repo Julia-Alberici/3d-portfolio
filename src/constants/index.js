c6a54ca983228193cc387ee90abc409950a49c39
Julia Alberici <julia.jpa0@gmail.com>
2023-04-10 21:39:36 -0300

    fix experiences info

--- src/constants/index.js
+++ src/constants/index.js
@@ -19,6 +19,7 @@ import {
     meta,
     starbucks,
     tesla,
+    korp,
     shopify,
     carrent,
     jobit,
@@ -121,57 +122,31 @@ import {
   
   const experiences = [
     {
-      title: "React.js Developer",
-      company_name: "Starbucks",
-      icon: starbucks,
-      iconBg: "#383E56",
-      date: "March 2020 - April 2021",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "React Native Developer",
-      company_name: "Tesla",
-      icon: tesla,
-      iconBg: "#E6DEDD",
-      date: "Jan 2021 - Feb 2022",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
-      title: "Web Developer",
-      company_name: "Shopify",
-      icon: shopify,
-      iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
-      points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-        "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
-      ],
-    },
-    {
       title: "Full stack Developer",
-      company_name: "Meta",
-      icon: meta,
+      company_name: "Visoft Korp",
+      icon: korp,
       iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
+      date: "Nov 2022 - Present",
       points: [
-        "Developing and maintaining web applications using React.js and other related technologies.",
-        "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+        "Designed, developed, and maintained software applications using mostly Typescript, React, Angular and C# .NET.",
+        "Contributed to the design and implementation of new features to improve the overall user experience and increase product functionality.",
         "Implementing responsive design and ensuring cross-browser compatibility.",
-        "Participating in code reviews and providing constructive feedback to other developers.",
+        "Mentored Intern developers, providing support to improve their technical skills.",
       ],
     },
+    {
+      title: "Full stack Intern Developer",
+      company_name: "Visoft Korp",
+      icon: korp,
+      iconBg: "#E6DEDD",
+      date: "July 2022 - Nov 2022",
+      points: [
+        "Developing and maintaining web applications using React.js and other related technologies.",
+        "Debugged and resolved software issues in a timely manner, ensuring the stability of software applications.",
+        "Implementing responsive design and ensuring cross-browser compatibility.",
+        "Implemented automated testing processes to increase the reliability and efficiency of software delivery.",
+      ],
+    }
   ];
   
   const testimonials = [
