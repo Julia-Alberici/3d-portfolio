11e486b3cf0a6bdfc0ec57d4b1e81134f7786851
Julia Alberici <julia.jpa0@gmail.com>
2023-04-13 22:00:23 -0300

    refactor balls to render only one canvas

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,74 @@
-import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, View } from "@react-three/drei";
+import CanvasLoader from "./Loader";
+import useRefs from "react-use-refs";
+import Ball from "./canvas/Ball";
 
 const Tech = () => {
+    const [
+        ref,
+        view0,
+        view1,
+        view2,
+        view3,
+        view4,
+        view5,
+        view6,
+        view7,
+        view8,
+        view9,
+        view10,
+        view11,
+        view12,
+        view13,
+    ] = useRefs();
+
+    const ballsRef = [
+        view0,
+        view1,
+        view2,
+        view3,
+        view4,
+        view5,
+        view6,
+        view7,
+        view8,
+        view9,
+        view10,
+        view11,
+        view12,
+        view13,
+    ];
+
     return (
-        <div className="flex flex-row flex-wrap justify-center gap-10">
-            {technologies.map((technology) => (
-                <div className="w-28 h-28" key={technology.name}>
-                    <BallCanvas icon={technology.icon} />
-                </div>
-            ))}
-        </div>
+        <>
+            <div
+                ref={ref}
+                className="flex flex-row flex-wrap justify-center gap-10">
+                {ballsRef.map((ballRef, i) => (
+                    <div key={i} ref={ballRef} className="w-28 h-28" />
+                ))}
+                <Canvas
+                    eventSource={ref}
+                    style={{ position: "fixed", top: "0px", left: "0px" }}>
+                    <Suspense fallback={<CanvasLoader />}>
+                        {ballsRef.map((ballRef, i) => (
+                            <View
+                                key={technologies[i].name + i}
+                                track={ballRef}>
+                                <Ball imgUrl={technologies[i].icon} />
+                                <OrbitControls enableZoom={false} />
+                            </View>
+                        ))}
+                    </Suspense>
+
+                    <Preload all />
+                </Canvas>
+            </div>
+        </>
     );
 };
 
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,13 +1,8 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
 import {
     Decal,
     Float,
-    OrbitControls,
-    Preload,
     useTexture,
 } from "@react-three/drei";
-import CanvasLoader from "../Loader";
 
 const Ball = (props) => {
     const [decal] = useTexture([props.imgUrl]);
@@ -34,17 +29,4 @@ const Ball = (props) => {
     );
 };
 
-const BallCanvas = ({ icon }) => {
-    return (
-        <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
-            <Suspense fallback={<CanvasLoader />}>
-                <OrbitControls enableZoom={false} />
-                <Ball imgUrl={icon} />
-            </Suspense>
-
-            <Preload all />
-        </Canvas>
-    );
-};
-
-export default BallCanvas;
+export default Ball;
